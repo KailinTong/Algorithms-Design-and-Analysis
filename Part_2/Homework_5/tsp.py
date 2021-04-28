ce75c3ae1fe53b5579dbfb8f4c34fadc5f00ccba
Tong, Kailin <kailin.tong@v2c2.at>
2021-04-28 13:41:16 +0200

    homework 5 update, the memory is the challenge!

--- Part_2/Homework_5/tsp.py
+++ Part_2/Homework_5/tsp.py
@@ -1,12 +1,52 @@
 import numpy as np
-from math import sqrt
+from math import sqrt, factorial
 from itertools import combinations
 from copy import deepcopy
-
+import gc
 
 def euclidean_distance(p1, p2):
     return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
+def n_choose_k(n, k):
+    """Computes n choose k."""
+
+    if n < k:
+        return 0
+    else:
+        return factorial(n) / factorial(k) / factorial(n - k)
+
+
+def n_choose_k_table(num_of_cities):
+    """Computes a dictionary which caches all possible n choose k computations."""
+    table = {}
+    for n in range(num_of_cities):
+        for k in range(1, n + 2):
+            table[(n, k)] = n_choose_k(n, k)
+    return table
+
+
+def comb_index(comb_start, comb, nck_table):
+    """
+    Computes a dense index for a given subset 'comb' in all subsets that have
+    the same number of elements as 'comb'. 'comb_start' is the minimum element in
+    the set from which subset 'comb' is obtained.
+
+    http://en.wikipedia.org/wiki/Combinatorial_number_system
+    copied from https://github.com/ChuntaoLu/Algorithms-Design-and-Analysis
+      comb        index
+    (1, 2, 3)  -->  0
+    (1, 2, 4)  -->  1
+    (1, 2, 5)  -->  4
+    (1, 3, 4)  -->  2
+    (1, 3, 5)  -->  5
+    (1, 4, 5)  -->  7
+    (2, 3, 4)  -->  3
+    (2, 3, 5)  -->  6
+    (2, 4, 5)  -->  8
+    (3, 4, 5)  -->  9
+    """
+    return sum([nck_table[(y - comb_start, x + 1)]
+                for x, y in enumerate(comb)])
 
 class TSP:
     def __init__(self, txt_name):
@@ -27,6 +67,8 @@ class TSP:
         INF = 99999999
         city_index = [i for i in range(0, self.n_cities)]
         costs = {}
+        new_costs = {}
+        nck_table = n_choose_k_table(self.n_cities)
         print("Initialization done!")
         for m in range(2, self.n_cities + 1):  # sub problem size including 0
             print("solving subproblem size ", m)
@@ -40,19 +82,27 @@ class TSP:
                     for k in smaller_s:
                         cost_k_des = []
                         if k != 0:
-                            cost_k_des.append(costs[tuple(smaller_s), k] + euclidean_distance(self.cities[k], self.cities[j]))
+                            smaller_c_index = comb_index(0, tuple(smaller_s), nck_table)
+                            cost_k_des.append(costs[smaller_c_index, k] + euclidean_distance(self.cities[k], self.cities[j]))
                         else:
                             if smaller_s == [0]:
                                 cost_k_des.append(0 + euclidean_distance(self.cities[0], self.cities[j]))
                             else:
                                 cost_k_des.append(INF)
-                    costs[(tuple(s), j)] = min(cost_k_des)
-                    # destination 0 will unavoidably cause visiting the vertex twice, such it is removed.
+                    new_c_index = comb_index(0, tuple(s), nck_table)
+                    new_costs[(new_c_index, j)] = min(cost_k_des)
+                    del cost_k_des
+                gc.collect()
+            costs = deepcopy(new_costs)
+            new_costs.clear()
+            gc.collect()
+
+            # destination 0 will unavoidably cause visiting the vertex twice, such it is removed.
         min_cost = min(
-            [costs[tuple(s), j] + euclidean_distance(self.cities[j], self.cities[0]) for j in range(1, self.n_cities)])
+            [costs[0, j] + euclidean_distance(self.cities[j], self.cities[0]) for j in range(1, self.n_cities)])
         return min_cost
 
 
 if __name__ == "__main__":
-    tsp = TSP('tsp_test.txt')
+    tsp = TSP('tsp.txt')
     print(tsp.solve_tsp())
